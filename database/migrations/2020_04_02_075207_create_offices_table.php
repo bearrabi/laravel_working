ebbea6d3a35e7f76ee648a2f9bf7ac9e73aca1bf
bearrabi <bearrabi0506@gmail.com>
2020-04-02 07:57:10 +0000

    edit companies table column

--- database/migrations/2020_04_02_075207_create_offices_table.php
+++ database/migrations/2020_04_02_075207_create_offices_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateCompaniesTable extends Migration
+class CreateOfficesTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,10 +13,9 @@ class CreateCompaniesTable extends Migration
      */
     public function up()
     {
-        Schema::create('companies', function (Blueprint $table) {
-            $table->bigIncrements('id');
-            $table->string('name');
-            $table->unsignedInteger('mainoffice_id');
+        Schema::create('offices', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
         });
     }
 
@@ -27,6 +26,6 @@ class CreateCompaniesTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('companies');
+        Schema::dropIfExists('offices');
     }
 }
