5efe8ab027bb4574fcf8cccbbe0b74989b9bbc6b
bearrabi <bearrabi0506@gmail.com>
2020-04-02 08:17:04 +0000

    add base info to departments_table_seeder

---
+++ database/seeds/DepartmentsTableSeeder.php
@@ -0,0 +1,21 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class DepartmentsTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        //
+        DB::table('departments')->insert([
+            'name' => 'kuma',
+            'email' => 'kumasanno@gmail.com',
+            'password' => Hash::make('kumasanno')
+        ]);
+    }
+}
