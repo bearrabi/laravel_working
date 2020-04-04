18ee7c84da349001d3726632dae6d2cc8106a8db
bearrabi <bearrabi0506@gmail.com>
2020-04-04 08:48:35 +0000

    repair OfficesTableSeeder.php

--- database/seeds/OfficesTableSeeder.php
+++ database/seeds/OfficesTableSeeder.php
@@ -13,18 +13,19 @@ class OfficesTableSeeder extends Seeder
     {
         //
         DB::table('offices')->insert([
-            'company_id' => 1,
-            'name' => 'クマ支社',
-            'post_number' =>'123-4567',
-            'address' =>'bear県kuma市サケ町123番地4',
-            'telnumber' => '09098989898'
-        ],[
-            'company_id' => 1,
-            'name' => 'ウサギ支社',
-            'post_number' =>'789-1234',
-            'address' =>'rabi県usagi市ninnjin町5番地67',
-            'telnumber' => '09093939393'
-        ]
-    );
+            [
+                'company_id' => 1,
+                'name' => 'クマ支社',
+                'post_number' =>'123-4567',
+                'address' =>'bear県kuma市サケ町123番地4',
+                'telnumber' => '09098989898',
+            ],[
+                'company_id' => 1,
+                'name' => 'ウサギ支社',
+                'post_number' =>'789-1234',
+                'address' =>'rabi県usagi市ninnjin町5番地67',
+                'telnumber' => '09093939393',
+            ]
+        ]);
     }
 }
