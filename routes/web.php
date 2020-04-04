bb4ec5b5625e0d6da177c880acf9aa6e9b8f38ff
bearrabi <bearrabi0506@gmail.com>
2020-04-04 08:01:20 +0000

    add Office@index to route

--- routes/web.php
+++ routes/web.php
@@ -18,5 +18,7 @@ Route::get('/', function () {
 });
 
 Auth::routes();
+Route::resource('offices','OfficeController')->only([
+    'index'
+]);
 
-Route::get('/home', 'HomeController@index')->name('home');
