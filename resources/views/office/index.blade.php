763f99f99be67b3dce792a533686ad6ef637b513
bearrabi <bearrabi0506@gmail.com>
2020-04-05 00:41:58 +0000

    edit office.index to layout

--- resources/views/office/index.blade.php
+++ resources/views/office/index.blade.php
@@ -1,21 +1,15 @@
-<!DOCTYPE html>
-<html lang="ja">
-<head>
-  <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Document</title>
-</head>
+@extends('layouts.app')
+
+@section('content')
 <style>
   #col1,#col2{  height: 30px;  }
   #col1{background-color:red; }
   #col2{background-color:green; }
 </style>
-<body>
-  <div class="container">
-    <div class="row">
-      <div id="col1" class="col-lg-6"></div>
-      <div id="col2" class="col-lg-6"></div>
-    </div>
+<div class="container">
+  <div class="row">
+    <div id="col1" class="col-lg-6"></div>
+    <div id="col2" class="col-lg-6"></div>
   </div>
-</body>
-</html>
+</div>
+@endsection
