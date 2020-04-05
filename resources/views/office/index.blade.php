0189ea1e57ebcf1d35feaa3cb92cbebf70bdb4bb
bearrabi <bearrabi0506@gmail.com>
2020-04-05 01:11:26 +0000

    check bootstrap to office.index

--- resources/views/office/index.blade.php
+++ resources/views/office/index.blade.php
@@ -8,8 +8,27 @@
 </style>
 <div class="container">
   <div class="row">
-    <div id="col1" class="col-lg-6"></div>
-    <div id="col2" class="col-lg-6"></div>
+    <table class="table table-striped">
+      <thead class="thead-dark">
+        <tr><th scope="col">事業所名</th><th scope="col">郵便番号</th><th scope="col">所在地</th><th scope="col">電話番号</th><th scope="col">操作</th></tr>
+      </thead>
+      <tbody>
+        <tr>
+          <td>kuma</td>
+          <td>123-4567</td>
+          <td>kuma県</td>
+          <td>0909898989898</td>
+          <td><a class="btn btn-primary" href="#">編集</a><a class="btn btn-danger" href="#">削除</a></td>
+        </tr>
+        <tr>
+          <td>usagi</td>
+          <td>789-1234</td>
+          <td>usagi県</td>
+          <td>0909393939393</td>
+          <td><a class="btn btn-primary" href="#">編集</a><a class="btn btn-danger" href="#">削除</a></td>
+        </tr>
+      </tbody>
+    </table>
   </div>
 </div>
 @endsection
