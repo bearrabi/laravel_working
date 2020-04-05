6f9e6f2819f38f6f655e151d7c19abcf5f9edcb0
bearrabi <bearrabi0506@gmail.com>
2020-04-05 00:28:43 +0000

    create new file index.blade.php

---
+++ resources/views/office/index.blade.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Document</title>
+</head>
+<style>
+  .col-lg-6{
+    height: 30px;
+    background-color:red;
+  }
+</style>
+<body>
+  <div class="container">
+  <div class="row">
+  <div class="col-lg-6">
+  </div>
+  <div class="col-lg-6">
+  </div>
+  </div>
+  </div>
+</body>
+</html>
