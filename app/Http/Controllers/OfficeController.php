4ac2c580dd4685e4a62131339c6161ab8e79b861
bearrabi <bearrabi0506@gmail.com>
2020-04-05 00:02:03 +0000

    add OfficeController@create return

--- app/Http/Controllers/OfficeController.php
+++ app/Http/Controllers/OfficeController.php
@@ -14,7 +14,7 @@ class OfficeController extends Controller
      */
     public function index()
     {
-        return 'indexです';
+        return 'Office@indexです';
     }
 
     /**
@@ -24,7 +24,7 @@ class OfficeController extends Controller
      */
     public function create()
     {
-        //
+        return 'Office@cretaeです';
 
     }
 
