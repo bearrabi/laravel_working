7c5d562a034526e20c88d82b37d949ba6cc1b85f
bearrabi <bearrabi0506@gmail.com>
2020-04-05 00:38:10 +0000

    edit office.index

--- resources/views/office/index.blade.php
+++ resources/views/office/index.blade.php
@@ -6,19 +6,16 @@
   <title>Document</title>
 </head>
 <style>
-  .col-lg-6{
-    height: 30px;
-    background-color:red;
-  }
+  #col1,#col2{  height: 30px;  }
+  #col1{background-color:red; }
+  #col2{background-color:green; }
 </style>
 <body>
   <div class="container">
-  <div class="row">
-  <div class="col-lg-6">
-  </div>
-  <div class="col-lg-6">
-  </div>
-  </div>
+    <div class="row">
+      <div id="col1" class="col-lg-6"></div>
+      <div id="col2" class="col-lg-6"></div>
+    </div>
   </div>
 </body>
 </html>
