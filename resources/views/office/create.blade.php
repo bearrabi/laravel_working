804e64a313074dd3a5252105982a3a183c056882
bearrabi <bearrabi0506@gmail.com>
2020-04-07 11:31:57 +0000

    create view create.office

--- resources/views/office/create.blade.php
+++ resources/views/office/create.blade.php
@@ -6,19 +6,19 @@
 </style>
 <div class="container">
   <div class="row">
-    <form method="POST" action="{{ route('office.update', ['id' => $office->id]) }}">
+    <form method="POST" action="{{ route('office.store') }}">
     @csrf
       <div class="form-group">
         <label for="office_name">事業所名</label>
-        <input type="text" class="form-control txtbx" name="office_name" id="office_name" value="{{$office->name}}"><br><br>
+        <input type="text" class="form-control txtbx" name="office_name" id="office_name"><br><br>
         <label for="office_postnumber">郵便番号</label>
-        <input type="text" class="form-control txtbx" name="office_postnumber" id="office_postnumber" value="{{$office->post_number}}"><br><br>
+        <input type="text" class="form-control txtbx" name="office_postnumber" id="office_postnumber"><br><br>
         <label for="office_address">所在地</label>
-        <input type="text" class="form-control txtbx" name="office_address" id="office_address" value="{{$office->address}}"><br><br>
+        <input type="text" class="form-control txtbx" name="office_address" id="office_address"><br><br>
         <label for="office_telnumber">電話番号</label>
-        <input type="text" class="form-control txtbx" name="office_telnumber" id="office_telnumber" value="{{$office->telnumber}}"><br><br>
+        <input type="text" class="form-control txtbx" name="office_telnumber" id="office_telnumber"><br><br>
       </div>
-      <input class="btn btn-info" type="submit" value="更新">
+      <input class="btn btn-info" type="submit" value="登録">
     </form>
   </div>
 </div>
