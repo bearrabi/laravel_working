3c7523ed501a9bcdfbf99fcb6e0e2ca0a928c940
bearrabi <bearrabi0506@gmail.com>
2020-04-08 11:31:18 +0000

    add route department

--- routes/web.php
+++ routes/web.php
@@ -27,5 +27,14 @@ Route::group(['prefix' => 'office'], function(){
     Route::post('update/{id}', 'OfficeController@update')->name('office.update');
     Route::post('destroy/{id}', 'OfficeController@destroy')->name('office.destroy');
 });
+Route::group(['prefix' => 'department'], function(){
+    Route::get('index', 'OfficeController@index')->name('office.index');
+    Route::get('create', 'OfficeController@create')->name('office.create');
+    Route::post('store', 'OfficeController@store')->name('office.store');
+    Route::get('show/{id}', 'OfficeController@show')->name('office.show');
+    Route::get('edit/{id}', 'OfficeController@edit')->name('office.edit');
+    Route::post('update/{id}', 'OfficeController@update')->name('office.update');
+    Route::post('destroy/{id}', 'OfficeController@destroy')->name('office.destroy');
+});
 
 
