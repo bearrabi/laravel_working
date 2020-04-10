c03e3cd43945cdf7fb67223a8179dfc5b61ecfb6
bearrabi <bearrabi0506@gmail.com>
2020-04-10 22:19:51 +0000

    repair DatabaseSeeder.php

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -13,6 +13,6 @@ class DatabaseSeeder extends Seeder
     {
         // $this->call(UserSeeder::class);
         $this->call(OfficesTableSeeder::class);
-        $this->call(DepartmentTableSeeder::class);
+        $this->call(DepartmentsTableSeeder::class);
     }
 }
