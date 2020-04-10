fba5fb69538160be55c48a56a71bc56d19ce075c
bearrabi <bearrabi0506@gmail.com>
2020-04-10 21:18:35 +0000

    edit department.index

--- resources/views/department/index.blade.php
+++ resources/views/department/index.blade.php
@@ -5,16 +5,14 @@
   <div class="row">
     <table class="table table-striped">
       <thead class="thead-dark">
-        <tr><th scope="col">事業所名</th><th scope="col">郵便番号</th><th scope="col">所在地</th><th scope="col">電話番号</th><th scope="col">操作</th></tr>
+        <tr><th scope="col">事業所名</th><th scope="col">部名</th><th scope="col">操作</th></tr>
       </thead>
       <tbody>
-      @foreach ($offices as $office)
+      @foreach ($departments as $dep)
         <tr>
-          <td>{{$office->name}}</td>
-          <td>{{$office->post_number}}</td>
-          <td>{{$office->address}}</td>
-          <td>{{$office->telnumber}}</td>
-          <td><a class="btn btn-primary" href="{{ action('OfficeController@edit', $office->id)}}">編集</a><a class="btn btn-danger" href="{{ action('OfficeController@destroy', $office->id)}}">削除</a></td>
+          <td>{{$dep->office->name}}</td>
+          <td>{{$dep->name}}</td>
+          <td><a class="btn btn-primary" href="{{ action('DepartmentController@edit', $dep->id)}}">編集</a><a class="btn btn-danger" href="{{ action('DepartmentController@destroy', $dep->id)}}">削除</a></td>
         </tr>
       @endforeach
       </tbody>
