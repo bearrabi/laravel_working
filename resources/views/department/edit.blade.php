d4069a69f65a18fa62c387cba804f8531503c19f
bearrabi <bearrabi0506@gmail.com>
2020-04-11 02:00:56 +0000

    edit view department.edit

--- resources/views/department/edit.blade.php
+++ resources/views/department/edit.blade.php
@@ -6,17 +6,13 @@
 </style>
 <div class="container">
   <div class="row">
-    <form method="POST" action="{{ route('office.update', ['id' => $office->id]) }}">
+    <form method="POST" action="{{ route('department.update', ['id' => $dep['id']]) }}">
     @csrf
       <div class="form-group">
-        <label for="office_name">事業所名</label>
-        <input type="text" class="form-control txtbx" name="office_name" id="office_name" value="{{$office->name}}"><br><br>
-        <label for="office_postnumber">郵便番号</label>
-        <input type="text" class="form-control txtbx" name="office_postnumber" id="office_postnumber" value="{{$office->post_number}}"><br><br>
-        <label for="office_address">所在地</label>
-        <input type="text" class="form-control txtbx" name="office_address" id="office_address" value="{{$office->address}}"><br><br>
-        <label for="office_telnumber">電話番号</label>
-        <input type="text" class="form-control txtbx" name="office_telnumber" id="office_telnumber" value="{{$office->telnumber}}"><br><br>
+      <label for="office_name">事業所名</label>
+        <input type="text" class="form-control txtbx" id="office_name" value="{{$dep['office_name']}}" readonly><br><br>
+        <label for="dep_name">部名</label>
+        <input type="text" class="form-control txtbx" id="dep_name" value="{{$dep['dep_name']}}" readonly><br><br>
       </div>
       <input class="btn btn-info" type="submit" value="更新">
     </form>
