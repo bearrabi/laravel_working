043623841df774021a3c04c962e803e597bd27ab
bearrabi <bearrabi0506@gmail.com>
2020-04-11 23:31:15 +0000

    edit view section.show

--- resources/views/section/show.blade.php
+++ resources/views/section/show.blade.php
@@ -9,15 +9,17 @@
     <form>
       <div class="form-group">
         <label for="office_name">事業所名</label>
-        <input type="text" class="form-control txtbx" id="office_name" value="{{$dep['office_name']}}" readonly><br><br>
+        <input name="office_name" type="text" class="form-control txtbx" id="office_name" value="{{$sep['office_name']}}" readonly><br><br>
         <label for="dep_name">部名</label>
-        <input type="text" class="form-control txtbx" id="dep_name" value="{{$dep['dep_name']}}" readonly><br><br>
+        <input name="dep_name" type="text" class="form-control txtbx" id="dep_name" value="{{$sec['dep_name']}}" readonly><br><br>
+        <label for="sec_name">課名</label>
+        <input name="sec_name" type="text" class="form-control txtbx" id="sec_name" value="{{$sec['sec_name']}}" readonly><br><br>
       </div>
     </form>
   </div>
   <div class="row">
-    <a class="btn btn-primary" href="{{ action('DepartmentController@edit', $dep['id'])}}">編集</a>
-    <form id="delete" method="POST" action="{{ route('department.destroy', ['id' => $dep['id']])}}">
+    <a class="btn btn-primary" href="{{ action('SectionController@edit', $sec['sec_id'])}}">編集</a>
+    <form id="delete" method="POST" action="{{ route('section.destroy', ['id' => $sec['sec_id']])}}">
     @csrf
       <input type="submit" class="btn btn-danger" value="削除" onClick="delete_alert(event); return false;">
     </form>
