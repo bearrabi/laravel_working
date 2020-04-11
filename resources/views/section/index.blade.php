52757eb4a6a502eaeba6648dd743ca9ae2d52d54
bearrabi <bearrabi0506@gmail.com>
2020-04-11 23:17:52 +0000

    edit view section.index

--- resources/views/section/index.blade.php
+++ resources/views/section/index.blade.php
@@ -5,18 +5,19 @@
   <div class="row">
     <table class="table table-striped">
       <thead class="thead-dark">
-        <tr><th scope="col">事業所名</th><th scope="col">部名</th><th scope="col">操作</th></tr>
+        <tr><th scope="col">事業所名</th><th scope="col">部名</th><th scope="col">課名</th><th scope="col">操作</th></tr>
       </thead>
       <tbody>
-      @foreach ($deps as $dep)
+      @foreach ($sections as $sec)
         <tr>
-          <td>{{$dep['office_name']}}</td>
-          <td>{{$dep['dep_name']}}</td>
+          <td>{{$sec['office_name']}}</td>
+          <td>{{$sec['dep_name']}}</td>
+          <td>{{$sec['sec_name']}}</td>
           <td>
             <div class="container">
             <div class="row">
-              <a class="btn btn-primary" href="{{ action('DepartmentController@edit', $dep['dep_id'])}}">編集</a>
-              <form id="delete" method="POST" action="{{ route('department.destroy', ['id' => $dep['dep_id']])}}">
+              <a class="btn btn-primary" href="{{ action('SectionController@edit', $sec['sec_id'])}}">編集</a>
+              <form id="delete" method="POST" action="{{ route('section.destroy', ['id' => $sec['sec_id']])}}">
                 @csrf
                 <input type="submit" class="btn btn-danger" value="削除" onClick="delete_alert(event); return false;">
               </form>
