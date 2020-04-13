11da2c2554de046127820f4097949be2e55f84f2
bearrabi <bearrabi0506@gmail.com>
2020-04-13 10:40:55 +0000

    edit view section.create

--- resources/views/section/create.blade.php
+++ resources/views/section/create.blade.php
@@ -9,14 +9,20 @@
     <form method="POST" action="{{ route('department.store') }}">
     @csrf
       <div class="form-group">
-      <label for="office_name">事業所名</label>
-        <select name="office_name" id="office_name" class="form-control selectbx">
-          @foreach($off_names as $off_name)
-            <option value="{{$off_name}}">{{$off_name}}</option>
-          @endforeach
-        </select><br><br>
+        <label for="office_name">事業所名</label>
+          <select name="office_name" id="office_name" class="form-control selectbx">
+            @foreach($off_names as $off_name)
+              <option value="{{$off_name}}">{{$off_name}}</option>
+            @endforeach
+          </select><br><br>
         <label for="dep_name">部名</label>
-        <input name="dep_name" type="text" class="form-control txtbx" id="dep_name"><br><br>
+          <select name="dep_name" id="dep_name" class="form-control selectbx">
+            @foreach($dep_names as $dep_name)
+              <option value="{{$dep_name}}">{{$dep_name}}</option>
+            @endforeach
+          </select><br><br>
+        <label for="sec_name">課名</label>
+        <input name="sec_name" type="text" class="form-control txtbx" id="sec_name"><br><br>
       </div>
       </div>
       <input class="btn btn-info" type="submit" value="登録">
