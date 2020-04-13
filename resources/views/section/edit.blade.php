6a21dd45212fd7c1a89a3a42cd2b86e4d38f93d7
bearrabi <bearrabi0506@gmail.com>
2020-04-13 11:09:31 +0000

    edit view section.edit

--- resources/views/section/edit.blade.php
+++ resources/views/section/edit.blade.php
@@ -6,13 +6,17 @@
 </style>
 <div class="container">
   <div class="row">
-    <form method="POST" action="{{ route('department.update', ['id' => $dep['id']]) }}">
+    <form method="POST" action="{{ route('section.update', ['id' => $sec['id']]) }}">
     @csrf
       <div class="form-group">
-      <label for="office_name">事業所名</label>
-        <input name="office_name" type="text" class="form-control txtbx" id="office_name" value="{{$dep['office_name']}}"><br><br>
+      <div class="form-group">
+        <label for="office_name">事業所名</label>
+        <input name="office_name" type="text" class="form-control txtbx" id="office_name" value="{{$sec['office_name']}}" readonly><br><br>
         <label for="dep_name">部名</label>
-        <input name="dep_name" type="text" class="form-control txtbx" id="dep_name" value="{{$dep['dep_name']}}"><br><br>
+        <input name="dep_name" type="text" class="form-control txtbx" id="dep_name" value="{{$sec['dep_name']}}" readonly><br><br>
+        <label for="sec_name">課名</label>
+        <input name="sec_name" type="text" class="form-control txtbx" id="sec_name"><br><br>
+      </div>
       </div>
       <input class="btn btn-info" type="submit" value="更新">
     </form>
